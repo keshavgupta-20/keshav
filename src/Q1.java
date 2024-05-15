c43c4b89e52ef4d684dca8c4683c3c2e15fc2814
keshavgupta-20 <guptakeshav20092003@gmail.com>
2024-05-16 03:37:09 +0530

    Keshav First repo

--- src/Q1.java
+++ src/Q1.java
@@ -1,7 +1,18 @@
+import java.util.*;
 public class Q1 {
     public static void main(String[] args) {
-        int a =3;
-        int b =34;
-        System.out.println(a+b);
+        Scanner sc = new Scanner(System.in);
+        int arr[] = new int[6];
+        int sum =0;
+        for (int i=1; i<=5; i++){
+            System.out.println("The marks of subject "+ i);
+            arr[i] = sc.nextInt();
+            sum = sum + arr[i];
+        }
+        System.out.println("Total marks:-" + sum + " out of 500");
+        System.out.println("Percentage:-" + sum/5);
+
+
+
     }
 }
